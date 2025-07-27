49d28c7421ba0cc9dd2797f1f9c7ec92755e7919
Mahesh-Bisunke <maheshbisunke92@gmail.com>
2025-07-27 11:55:03 +0545

    Learning Management System Backend started

---
+++ learningmanagement-backend/src/main/java/com/learningmgmt/LearningmanagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learningmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmanagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningmanagementBackendApplication.class, args);
+	}
+
+}
